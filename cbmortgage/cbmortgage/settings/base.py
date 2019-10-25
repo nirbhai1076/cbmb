e9a8796addc28c6fc4a82473228553f489bccc2b
nirbhai sidhu <nss@nirbhais-MBP.hitronhub.home>
2019-10-25 00:30:37 -0600

    added staic files dir to setting

--- cbmortgage/cbmortgage/settings/base.py
+++ cbmortgage/cbmortgage/settings/base.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "my_project", "static"), )
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL='/media/'
