027ee5f905b1f34b45572a245408e921d09a8cb4
nirbhai sidhu <nss@nirbhais-MBP.hitronhub.home>
2019-10-25 11:00:44 -0600

    no comment

--- cbmortgage/cbmortgage/settings/base.py
+++ cbmortgage/cbmortgage/settings/base.py
@@ -3,7 +3,7 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 
@@ -105,10 +105,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join (BASE_DIR, "static")
-# Media 
-MEDIA_URL='/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
+STATICFILES_DIRS = (os.path.join(
+    BASE_DIR, "cbmortgage", "static"),)
+STATIC_ROOT = os.path.join(
+    os.path.dirname(BASE_DIR), "deployment", "collected_static")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(
+    os.path.dirname(BASE_DIR), "deployment", "media")
 
